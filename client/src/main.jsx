456fbf0ff1cae2b00781a07a7e9f1ebd52aa3dd1
jobayer561 <jobayer3085@gmail.com>
2025-11-06 21:36:51 +0600

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,70 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from "./Layout/RootLayout.jsx";
+import Home from "./Components/Home.jsx";
+import AllProducts from "./Components/AllProducts.jsx";
+import Register from "./Components/Register.jsx";
+import MyProducts from "./Components/MyProducts.jsx";
+import MyBids from "./Components/MyBids.jsx";
+import ProductDetails from "./Components/ProductDetails.jsx";
+import PrivateRoute from "./Router/PrivateRoute.jsx";
+import AuthProvider from "./Context/AuthProvider.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "allProducts",
+        Component: AllProducts,
+      },
+      {
+        path: "register",
+        Component: Register,
+      },
+      {
+        path: "myProducts",
+        element: (
+          <PrivateRoute>
+            <MyProducts></MyProducts>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "myBids",
+        element: (
+          <PrivateRoute>
+            <MyBids></MyBids>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "productDetails/:id",
+        loader: ({ params }) =>
+          fetch(`http://localhost:3000/products/${params.id}`),
+        element: (
+          <PrivateRoute>
+            <ProductDetails></ProductDetails>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
